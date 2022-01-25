3416b597c21703f45fe522e1f7703f311ab97fd0
Elstan Lewis <elstanlewis7@gmail.com>
2022-01-24 20:26:16 -0500

    Testing voice command-passed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey =
+  'c22bde11054ebd8fd04eb0f66d26bfcd2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === 'testCommand') {
+          alert('This code was executed!');
+        }
+      },
+    });
+  }, []);
   return (
     <div>
       <h1>Alan AI News Application</h1>
