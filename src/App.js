8c4a1c82df4a4015a2da8505b0faa270dddef818
Elstan Lewis <elstanlewis7@gmail.com>
2022-01-24 21:38:31 -0500

    Material UI for styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey =
   'c22bde11054ebd8fd04eb0f66d26bfcd2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
-        if (command === 'testCommand') {
-          alert('This code was executed!');
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
         }
       },
     });
   }, []);
+
   return (
     <div>
       <h1>Alan AI News Application</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 };
